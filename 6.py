673e325c21c762af1ac24ca80a2a7450a528ec1e
nioomeee <nioomeee@gmail.com>
2026-02-13 20:25:09 +0530

    printing dictionary

--- 6.py
+++ 6.py
@@ -14,3 +14,13 @@ print(scores.get("Donald"))
 scores["Goofy"] = 98
 
 print(scores.get("Goofy"))
+
+for name, score in scores.items():
+    print(name + " got " + str(score))
+
+# Write a loop that prints everyone's name and score.
+# Bonus Challenge: Inside the loop, check if the score is greater than 90. If it is, print "[Name] passed!".
+
+for name, score in scores.items():
+    if score > 90:
+        print(name + " passed")
