c3a87b39ed6f866bc7219d87f08e494dce1dc13e
nioomeee <nioomeee@gmail.com>
2026-02-13 20:01:57 +0530

    *2

---
+++ 2.py
@@ -0,0 +1,7 @@
+# You have a list of numbers: nums = [1, 2, 3, 4].
+# I want you to make a new list where every number is multiplied by 2.
+nums = [1, 2, 3, 4]
+
+new_list = [num*2 for num in nums]
+
+print(new_list)
