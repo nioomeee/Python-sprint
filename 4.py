96d435dd1953b2ff8bcd7059629d74fc5e590b7e
nioomeee <nioomeee@gmail.com>
2026-02-13 20:12:11 +0530

    Slicing

---
+++ 4.py
@@ -0,0 +1,20 @@
+# Slicing
+# [start: stop : step]
+
+s = "Niomi"
+
+print(s[0:3])
+
+print(s[1:])
+
+print(s[::-1])
+
+data = [10, 20, 30, 40, 50, 60]
+
+# Get the first 3 numbers (10, 20, 30).
+
+print(data[:3])
+
+# Get the last 2 numbers (50, 60).
+
+print(data[-2:])
